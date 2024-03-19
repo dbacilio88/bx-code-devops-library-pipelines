27456e2015fbfdf74e99e9fca006c4e03cb73778
cbaciliodev <dbacilio88@outlook.es>
2024-03-19 11:39:24 -0500

    fix pipeline step to step

--- vars/bxcodePipelineGit.groovy
+++ vars/bxcodePipelineGit.groovy
@@ -7,7 +7,7 @@ def call(PipelineBuilder builder, boolean wout, boolean smc, String version) {
     def solutionProject = ""
 
     pipeline {
-        agent none
+        agent any
         tools {
             maven: 'maven'
             gradle: 'gradle'
